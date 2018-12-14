a23e2a603a039a0a1077d37c25ff2b2ff2c974b5
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-12-14 22:35:24 +0530

    poc of which is loaded in java memory

---
+++ SecondJar/src/main/java/com/himanshu/poc/Class1.java
@@ -0,0 +1,18 @@
+package com.himanshu.poc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Hello world!
+ */
+public class Class1 {
+    public static void main(String[] args) {
+        Class2 class2 = new Class2();
+        class2.method();
+    }
+}
